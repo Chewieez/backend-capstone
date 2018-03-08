ecd9fe7cb9b721af36295570a33d635a1c9231d0
Chewieez <gregaudio@gmail.com>
2018-03-08 09:48:23 -0800

    geolocation coordinates are now aquired and stored when a new store is created in db

--- RepPortal/wwwroot/js/site.js
+++ RepPortal/wwwroot/js/site.js
@@ -17,3 +17,28 @@ function initMap() {
         map: map
     });
 }
+
+//get geocode data of the new store upon creation 
+$("#CreateStoreBtn").click(evt => {
+    evt.preventDefault()
+    //ajax request to get location
+    const address = $("#formStreetAddress").val()
+    const city = $("#formCity").val()
+    //const state = $("#formState").val()
+    const zip = $("#formZipcode").val()
+    $.ajax({
+        method: "GET",
+        url: `https://maps.googleapis.com/maps/api/geocode/json?address=${address}+${city}+${zip}&key=${googleAPI.key}`
+    }).then(res => {
+        //geolocation of the address entered
+        let geoLocation = res.results["0"].geometry.location
+
+        //assign to position input
+        //let geo = JSON.stringify(geoLocation)
+        $("#Map-Lat").val(geoLocation.lat)
+        $("#Map-Long").val(geoLocation.lng)
+
+        //submit form
+        $('form').submit()
+    })
+})
